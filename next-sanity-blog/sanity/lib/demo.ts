3372618d38ed778a57e09af4b5e9feef09e26cf6
Hassan Khan <khassaan139@gmail.com>
2025-01-09 22:06:18 -0800

    Update demo.ts

--- next-sanity-blog/sanity/lib/demo.ts
+++ next-sanity-blog/sanity/lib/demo.ts
@@ -2,7 +2,7 @@
  * Demo data used as placeholders and initial values for the blog
  */
 
-export const title = "Blog.";
+export const title = "Top 5 Games In The World";
 
 export const description = [
   {
@@ -13,46 +13,9 @@ export const description = [
         _key: "4a58edd077880",
         _type: "span",
         marks: [],
-        text: "A statically generated blog example using ",
-      },
-      {
-        _key: "4a58edd077881",
-        _type: "span",
-        marks: ["ec5b66c9b1e0"],
-        text: "Next.js",
-      },
-      {
-        _key: "4a58edd077882",
-        _type: "span",
-        marks: [],
-        text: " and ",
-      },
-      {
-        _key: "4a58edd077883",
-        _type: "span",
-        marks: ["1f8991913ea8"],
-        text: "Sanity",
-      },
-      {
-        _key: "4a58edd077884",
-        _type: "span",
-        marks: [],
-        text: ".",
+        text: "Top 5 Games In The World",
       },
     ],
-    markDefs: [
-      {
-        _key: "ec5b66c9b1e0",
-        _type: "link",
-        href: "https://nextjs.org/",
-      },
-      {
-        _key: "1f8991913ea8",
-        _type: "link",
-        href: "https://sanity.io/",
-      },
-    ],
-    style: "normal",
   },
 ];
 
